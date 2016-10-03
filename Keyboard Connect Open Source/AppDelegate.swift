17a6779dad37f75cec941fb9ecc879558f891398
Arthur Yidi <ayidi@arthurs-macbook.local>
2016-10-03 14:12:45 -0700

    Initial Commit

---
+++ Keyboard Connect Open Source/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Keyboard Connect Open Source
+//
+//  Created by Arthur Yidi on 10/3/16.
+//  Copyright © 2016 Arthur Yidi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
